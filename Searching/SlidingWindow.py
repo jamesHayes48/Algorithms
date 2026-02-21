5bfb77a3bfcc55c2686760377ef968ffefa477de
DingDongReallyFast <jhpipboy@gmail.com>
2026-02-21 12:37:17 -0700

    Example of fixed-length  sliding window

---
+++ Searching/SlidingWindow.py
@@ -0,0 +1,30 @@
+def sliding_max_subarray(arr, k):
+    '''
+    Used to find max subarray with a sliding window.
+
+    Kadane's Algorithm
+    :param arr:
+    :param k:
+    :return:
+    '''
+    window_sum = 0
+    max_result = -1
+    n = len(arr)
+
+    # Calculate first sum of the window size k
+    for i in range(0, k):
+        window_sum += arr[i]
+
+    # Initialize max sum
+    max_result = window_sum
+
+    # slide window
+    for i in range(k, n):
+        # Update window sum by subtracting element leaving the window from the left
+        # Add new element entering from the right
+        window_sum += arr[i] - arr[i - k]
+        max_result = max(max_result, window_sum)
+    return max_result
+
+my_array = [-1, 2, 5, -7, 6, 8, 7, -100]
+print(fixed_sliding_window(my_array, 3))
